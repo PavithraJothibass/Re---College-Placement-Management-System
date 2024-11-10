afeb6540bb219722a484217c012cba54ec6f95e6
PavithraJothibass <165559616+PavithraJothibass@users.noreply.github.com>
2024-11-10 13:39:52 +0530

    Update Register.jsx

--- frontend/src/components/Auth/Register.jsx
+++ frontend/src/components/Auth/Register.jsx
@@ -25,7 +25,7 @@ const Register = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/user/register",
+        "https://re-college-placement-management-system.onrender.com/api/v1/user/register",
         { name, phone, email, role, password },
         {
           headers: {
