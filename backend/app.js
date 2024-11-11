9d6bc026aa704e7bde49ac1f5f058eb50e59dcdd
PavithraJothibass <165559616+PavithraJothibass@users.noreply.github.com>
2024-11-11 17:39:32 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -13,11 +13,7 @@ const app = express();
 config({ path: "./config/config.env" });
 
 app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL],
-    method: ["GET", "POST", "DELETE", "PUT"],
-    credentials: true,
-  })
+  cors()
 );
 
 app.use(cookieParser());
